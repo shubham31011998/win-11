a8054f1b80078d6dc3af23447aa324bf9800d9ed
shubham31011998 <shubhamyaduvanshi03@gmail.com>
2021-11-27 22:03:59 +0530

    Install MUI + add Walpaper

---
+++ src/Desktop/Desktop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Desktop.css';
+
+const Desktop = () => {
+    return (
+        <div className="main_desktop" style={{ backgroundImage: "url(img/walpaper.jpg)",backgroundSize: "cover",backgroundPosition: "center" }}>
+            Desktop
+        </div>
+    )
+}
+export default Desktop
