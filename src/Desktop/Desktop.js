9bb1e83c1850a59882eb6033b13411d18083aa35
shubham31011998 <shubhamyaduvanshi03@gmail.com>
2021-11-27 22:22:11 +0530

    Add blank Taskbar(glassmorphism)

--- src/Desktop/Desktop.js
+++ src/Desktop/Desktop.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Desktop.css';
+import Taskbar from '../Taskbar/Taskbar';
 
 const Desktop = () => {
     return (
-        <div className="main_desktop" style={{ backgroundImage: "url(img/walpaper.jpg)",backgroundSize: "cover",backgroundPosition: "center" }}>
-            Desktop
+        <div className="main_desktop" style={{ backgroundImage: "url(img/walpaper.jpg)"}}>
+         <Taskbar />   
         </div>
     )
 }
---
+++ src/Taskbar/Taskbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Taskbar.css';
+
+const Taskbar = () => {
+    return (
+        <div className="taskbar_main">
+            
+        </div>
+    )
+}
+export default Taskbar
